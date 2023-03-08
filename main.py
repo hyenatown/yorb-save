49ffec12a278b75ebf050ccd26d54da054f5cf00
hyenatown <10572167+hyenatown@users.noreply.github.com>
2023-03-08 13:52:41 -0400

    threw together funny lil main menu

---
+++ main.py
@@ -0,0 +1,17 @@
+# This software is under a Yet-To-Be-Licensed (YTBL) meta-license.
+# Distribution, copying, or forking is still to be determined, so cool your jets.
+# That said, please feel free to contribute to the main project ("yorb-save"), until a license is finalized.
+
+#Includes
+# import numpy # If I decided to write the menu as an array.
+
+# This is the main bit that asks what you wanna do.
+print(
+    'Yorb 0.0.1-dev\n'
+    'Welcome! What do you wish to do?\n'
+    '[1] Check Links\n'
+    '[2] Delete Links\n'
+    '[3] Create New Archive\n'
+    '[4] Open Existing Archive'
+)
+exit()
