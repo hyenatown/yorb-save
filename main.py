98b5c2035e5db8f5ac875a51a1538c1829825fa4
hyenatown <10572167+hyenatown@users.noreply.github.com>
2023-03-11 01:05:23 -0400

    job selection function; create_symlink function

--- main.py
+++ main.py
@@ -15,21 +15,25 @@ print(
     '[4] Open Existing Archive\n'
     '[q] Exit'
 )
-# TODO: This is turing into an if-tree mess so I'll redo it later. It technically works.
+# TODO: This is turning into an if-tree mess so I'll redo it later. It technically works.
 def job_selection():
     job = ['q','1','2','3','4']
     job_key = input("Choose an option: ")
-
-if job_key == job[0]:
-    exit("Bye.")
-if job_key == job[1]:
-    print("This job is Check Paths")
-if job_key == job[2]:
-    print("This job is Delete Path")
-if job_key == job[3]:
-    print("This job is Link a Path")
-if job_key == job[4]:
-    print("This job is Open Existing Archive")
-
-print("Try again.")
+    if job_key == job[0]:
+        exit("Bye.")
+    if job_key == job[1]:
+        print("This job is Check Paths")
+        exit()
+    if job_key == job[2]:
+        print("This job is Delete Path")
+        exit()
+    if job_key == job[3]:
+        print("This job is Link a Path")
+        exit()
+    if job_key == job[4]:
+        print("This job is Open Existing Archive")
+        exit()
+    else:
+        print("Try again.")
+        job_selection()
 job_selection()
--- symlinker.py
+++ symlinker.py
@@ -22,7 +22,8 @@ def add_symlink():
         print('Moving file(s), creating symlink...')
 
 # Move the origin to the destination, and then replace the origin with a symlink.
-move(origin_path,dest_path)
-symlink(dest_path,origin_path)
-print('Done!\a')
-exit('Symlink created at: ' + origin_path)
+def create_symlink():
+    move(origin_path,dest_path) 
+    symlink(dest_path,origin_path)   
+    print('Done!\a')   
+    exit('Symlink created at: ' + origin_path)
